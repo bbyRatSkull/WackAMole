8bfafe1ee14b2ae4cc4c1763c7758fef535dedaa
Alyssa <alyssakhummel@gmail.com>
2025-09-03 14:19:21 -0400

    Fixed screen positioning issue

--- WAM1.py
+++ WAM1.py
@@ -16,8 +16,6 @@ def resource_path(relative_path):
 
     return os.path.join(base_path, relative_path)
 
-print("hello how does git work?")
-
 # Mole class
 class Mole(Sprite):
     def __init__(self, x, y):
@@ -38,18 +36,20 @@ red = (255,0,0)
 # set up the display
 pygame.init()
 # a list in the format [(x,y)]
-ratio_sizes = [(1280,720),(1504,846),(1920,1080)]
+ratio_sizes = [(1024, 576),(1280,720),(1504,846),(1920,1080)]
 screenvar = pygame.display.get_desktop_sizes()
+# for some reason this needs to be called here or the screen doesnt resize properly
+screen = pygame.display.set_mode((screenvar[0][0], screenvar[0][1]), FULLSCREEN)
+# this suto sets the game window to be the closest resolution
 for item in ratio_sizes:
     if screenvar[0][0] >= item[0] and screenvar[0][1] >= item[1]:
         GAME_SIZE = item
 #the coordinates for window generation
 GEN_COORDS = [((screenvar[0][0]-GAME_SIZE[0])/2), ((screenvar[0][1]-GAME_SIZE[1])/2)]
 
-
-screen = pygame.display.set_mode(GAME_SIZE, FULLSCREEN)
+#screen = pygame.display.set_mode(GAME_SIZE, FULLSCREEN) may not need this
 # loads in image and scales it to screen size
-BG = transform.scale(pygame.image.load("BG_Menu.PNG").convert(),(GAME_SIZE))
+BG = transform.scale(pygame.image.load("BG_Menu.PNG").convert(),GAME_SIZE)
 pygame.display.set_caption("Whack a Mole!")
 
 MOLE = transform.scale(pygame.image.load("Sprites/temp_hole.PNG").convert_alpha(),(72,72))
@@ -62,7 +62,8 @@ headerfont.set_bold(True)
 def main_menu(): #the main menu screen
     while True:
         screen.fill(black)
-        screen.blit(BG, (GEN_COORDS[0], GEN_COORDS[1]))
+        screen.blit(BG, (GEN_COORDS[0], GEN_COORDS[1])) 
+        #BG.get_rect(center = screen.get_rect().center) insted of coords works to center window
 
         mousePos = pygame.mouse.get_pos()
         mousex = mousePos[0]
